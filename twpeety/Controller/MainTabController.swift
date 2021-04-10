ad89ee745a2fe1d6f98639d139274c600061b5b0
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-04-10 08:34:22 +0100

    action button done

--- twpeety/Controller/MainTabController.swift
+++ twpeety/Controller/MainTabController.swift
@@ -10,16 +10,32 @@ import UIKit
 class MainTabController: UITabBarController {
     
     // MARK: - Properties
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named:"new_tweet"), for: .normal)
+        return button
+    }()
+    
+    
     
     // MARK: -  LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureUI()
 
     }
     
     // MARK: - Helpers
+    func configureUI() {
+        view.addSubview(actionButton)
+        actionButton.anchor( bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56/2
+    }
+    
      
     func configureViewController() {
 
